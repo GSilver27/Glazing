08c3215992a0eb6f5181b08e39b89457726cf63a
Silver <gramakov27@gmail.com>
2023-03-12 01:52:31 +0300

    ajax forms, regular expression for phone number

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,39 @@
+const tabs = (headerSelector, tabsSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+          tab = document.querySelectorAll(tabsSelector),
+          content = document.querySelectorAll(contentSelector);
+
+    function hideTabsContent() {
+        content.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabsContent(i = 0) {
+        content[i].style.display = 'block';
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabsContent();
+    showTabsContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target && 
+            (target.classList.contains(tabsSelector.replace(/\./, "")) || 
+            target.parentNode.classList.contains(tabsSelector.replace(/\./, "")))) {
+                tab.forEach((item, i) => {
+                    if (target == item || target.parentNode == item) {
+                        hideTabsContent();
+                        showTabsContent(i);
+                    }
+                });
+        }
+    });
+};
+
+export default tabs;
